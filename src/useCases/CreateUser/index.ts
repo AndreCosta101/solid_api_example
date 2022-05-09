9246a7256b5eb0a3887738280d1667d387e4f7ce
AndreCosta <andre.costa101@gmail.com>
2022-05-09 18:22:33 -0300

    all layers implemented

---
+++ src/useCases/CreateUser/index.ts
@@ -0,0 +1,20 @@
+
+import { CreateUserUseCase } from './CreateUserUseCase';
+
+
+import { MailTrapMailProvider } from "../../providers/implementations/MailTrapMailProvider";
+import { PostgresUsersRepository } from "../../repositories/implementations/PostgresUsersRepository";
+import { CreateUserController } from './CreateUserController';
+
+const mailTrapMailProvider = new MailTrapMailProvider();
+const postgresUsersRepository = new PostgresUsersRepository();
+
+const createUserUseCase = new CreateUserUseCase(
+    postgresUsersRepository, 
+    mailTrapMailProvider
+);
+
+const createUserController = new CreateUserController(createUserUseCase);
+
+
+export { createUserUseCase, createUserController}
